70be2073b65254819cae0265147e75dc3c2c5f47
sebastianzapatar <63472538+sebastianzapatar@users.noreply.github.com>
2022-04-27 11:28:58 -0500

    Se actualizan los cors y ponemos H2

--- src/main/java/com/backend/controllers/Controlador.java
+++ src/main/java/com/backend/controllers/Controlador.java
@@ -1,11 +1,12 @@
 package com.backend.controllers;
 
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.model.Producto;
 import com.backend.services.IProductoService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
+@CrossOrigin(origins = "*", 
+methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,
+		RequestMethod.DELETE})
 @RestController
 @RequestMapping("/api")
 public class Controlador {
